2441da3f52aa826aeeb2dd775991773756c546fc
DevExpressExampleBot <fakeEmail>
2019-12-19 09:19:48 +0300

    Source auto update [skip ci]

---
+++ VB/DXWebApplication21/Global.asax.vb
@@ -0,0 +1,29 @@
+﻿Imports DevExpress.DashboardWeb
+Imports System
+Imports System.Configuration
+
+Namespace DXWebApplication21
+	Public Class Global_asax
+		Inherits System.Web.HttpApplication
+
+		Private Sub Application_Start(ByVal sender As Object, ByVal e As EventArgs)
+			System.Web.Routing.RouteTable.Routes.MapPageRoute("defaultRoute", "", "~/Default.aspx")
+			AddHandler DevExpress.Web.ASPxWebControl.CallbackError, AddressOf Application_Error
+
+			ASPxDashboard.StaticInitialize()
+
+			DashboardConfigurator.Default.SetConnectionStringsProvider(New ConnectionStringsStorage(ConfigurationManager.ConnectionStrings("ConnectionsStorage").ConnectionString))
+
+			Dim dashboardFileStorage As New DashboardFileStorage("~/App_Data/Dashboards")
+			DashboardConfigurator.Default.SetDashboardStorage(dashboardFileStorage)
+		End Sub
+
+		Private Sub Application_End(ByVal sender As Object, ByVal e As EventArgs)
+			' Code that runs on application shutdown
+		End Sub
+
+		Private Sub Application_Error(ByVal sender As Object, ByVal e As EventArgs)
+			' Code that runs when an unhandled error occurs
+		End Sub
+	End Class
+End Namespace
---
+++ VB/DXWebApplication21/MyJsonConnection.vb
@@ -0,0 +1,22 @@
+﻿Imports System.ComponentModel.DataAnnotations
+Imports System.ComponentModel.DataAnnotations.Schema
+Imports System.Data.Entity
+
+Namespace DXWebApplication21
+	Public Class MyJsonConnection
+		<Key>
+		<DatabaseGenerated(DatabaseGeneratedOption.Identity)>
+		Public Property ID() As Integer
+		Public Property Name() As String
+		Public Property ConnectionString() As String
+	End Class
+
+	Friend Class JsonConnectionContext
+		Inherits DbContext
+
+		Public Property Connections() As DbSet(Of MyJsonConnection)
+		Public Sub New()
+			MyBase.New("ConnectionsStorage")
+		End Sub
+	End Class
+End Namespace
